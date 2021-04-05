c565cdc672f14c419c206df5539db38e6a235da4
marinadumberg <marina.dumberg@inbox.lv>
2021-04-06 00:13:05 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+const IngredientsLiEl = document.querySelector('ingredients-list');
+const elements = [];
+for (let i = 0; i < ingredients.length; i += 1){
+  
+  const liEl = document.createElement('li');
+  liEl.textContent = [ingredients];
+  elements.push(liEl);
+}
+console.log(elements);
